ce15379c3c42ca95d3823d8e326de5f53779c00f
Paul McKee <paul.mckee.aus@gmail.com>
2020-01-22 21:59:50 +1100

    More test tests

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,3 +1,5 @@
+import pytest
+
 # content of test_sample.py
 def func(x):
     return x + 1
@@ -5,3 +7,22 @@ def func(x):
 
 def test_answer():
     assert func(3) == 4
+
+
+def f():
+    raise SystemExit(1)
+
+
+def test_mytest():
+    with pytest.raises(SystemExit):
+        f()
+
+class TestClass:
+  def test_one(self):
+      x = "this"
+      assert "h" in x
+
+  def test_two(self):
+      x = "hello"
+      # assert hasattr(x, "check")
+      #assert hasattr(x, "x")
